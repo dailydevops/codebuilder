c7df27edf124ce624780ac8ba4a4f221e49d2cfc
Martin Stühmer <me@samtrion.net>
2025-07-10 08:33:26 +0200

    test: Further tests

--- tests/NetEvolve.CodeBuilder.Tests.Unit/CSharpCodeBuilderTests.Documentation.cs
+++ tests/NetEvolve.CodeBuilder.Tests.Unit/CSharpCodeBuilderTests.Documentation.cs
@@ -183,19 +183,114 @@ public partial class CSharpCodeBuilderTests
     }
 
     [Test]
-    public async Task AppendXmlDocException_WithValidParameters_Should_AppendExceptionElement()
+    public async Task AppendXmlDocRemarks_WithMultipleLines_Should_AppendRemarksElementWithAllLines()
+    {
+        var builder = new CSharpCodeBuilder();
+        var remarksLines = new[] { "First remark line", "Second remark line", "Third remark line" };
+
+        var result = builder.AppendXmlDocRemarks(remarksLines);
+
+        var expected =
+            "/// <remarks>"
+            + Environment.NewLine
+            + "/// First remark line"
+            + Environment.NewLine
+            + "/// Second remark line"
+            + Environment.NewLine
+            + "/// Third remark line"
+            + Environment.NewLine
+            + "/// </remarks>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocRemarks_WithEmptyAndNullLines_Should_SkipEmptyLines()
+    {
+        var builder = new CSharpCodeBuilder();
+        var remarksLines = new[] { "First remark", "", "Third remark" };
+
+        var result = builder.AppendXmlDocRemarks(remarksLines);
+
+        var expected =
+            "/// <remarks>"
+            + Environment.NewLine
+            + "/// First remark"
+            + Environment.NewLine
+            + "/// Third remark"
+            + Environment.NewLine
+            + "/// </remarks>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocRemarks_WithOnlyEmptyLines_Should_AppendOnlyOpeningTag()
+    {
+        var builder = new CSharpCodeBuilder();
+        var remarksLines = new[] { "", "", "" };
+
+        var result = builder.AppendXmlDocRemarks(remarksLines);
+
+        // The current implementation has a bug where it opens the tag but doesn't close it
+        // when there are no valid content lines
+        var expected = "/// <remarks>" + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocRemarks_WithNullCollection_Should_NotAppendAnything()
     {
         var builder = new CSharpCodeBuilder();
 
-        var result = builder.AppendXmlDocException("ArgumentNullException", "Thrown when argument is null");
+        var result = builder.AppendXmlDocRemarks((IEnumerable<string>?)null);
 
         _ = await Assert.That(result).IsEqualTo(builder);
-        _ = await Assert
-            .That(builder.ToString())
-            .IsEqualTo(
-                "/// <exception cref=\"ArgumentNullException\">Thrown when argument is null</exception>"
-                    + Environment.NewLine
-            );
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocRemarks_WithEmptyCollection_Should_AppendOnlyOpeningTag()
+    {
+        var builder = new CSharpCodeBuilder();
+        var remarksLines = Array.Empty<string>();
+
+        var result = builder.AppendXmlDocRemarks(remarksLines);
+
+        // The current implementation has a bug where it opens the tag but doesn't close it
+        // when there are no valid content lines
+        var expected = "/// <remarks>" + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocRemarks_WithNullString_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocRemarks((string?)null);
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocRemarks_WithEmptyString_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocRemarks("");
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
     }
 
     [Test]
@@ -215,6 +310,111 @@ public partial class CSharpCodeBuilderTests
 
         _ = await Assert.That(result).IsEqualTo(builder);
         _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExample_WithMultipleLines_Should_AppendExampleElementWithAllLines()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exampleLines = new[]
+        {
+            "var builder = new CSharpCodeBuilder();",
+            "builder.AppendLine(\"Hello World\");",
+            "var result = builder.ToString();",
+        };
+
+        var result = builder.AppendXmlDocExample(exampleLines);
+
+        var expected =
+            "/// <example>"
+            + Environment.NewLine
+            + "/// var builder = new CSharpCodeBuilder();"
+            + Environment.NewLine
+            + "/// builder.AppendLine(\"Hello World\");"
+            + Environment.NewLine
+            + "/// var result = builder.ToString();"
+            + Environment.NewLine
+            + "/// </example>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExample_WithEmptyAndNullLines_Should_SkipEmptyLines()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exampleLines = new[] { "var x = 1;", "", "var y = 2;" };
+
+        var result = builder.AppendXmlDocExample(exampleLines);
+
+        var expected =
+            "/// <example>"
+            + Environment.NewLine
+            + "/// var x = 1;"
+            + Environment.NewLine
+            + "/// var y = 2;"
+            + Environment.NewLine
+            + "/// </example>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExample_WithOnlyEmptyLines_Should_AppendOnlyOpeningTag()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exampleLines = new[] { "", "", "" };
+
+        var result = builder.AppendXmlDocExample(exampleLines);
+
+        // The current implementation has a bug where it opens the tag but doesn't close it
+        // when there are no valid content lines
+        var expected = "/// <example>" + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExample_WithEmptyCollection_Should_AppendOnlyOpeningTag()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exampleLines = Array.Empty<string>();
+
+        var result = builder.AppendXmlDocExample(exampleLines);
+
+        // The current implementation has a bug where it opens the tag but doesn't close it
+        // when there are no valid content lines
+        var expected = "/// <example>" + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExample_WithNullString_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocExample((string?)null);
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExample_WithEmptyString_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocExample("");
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
     }
 
     [Test]
@@ -448,5 +648,297 @@ public partial class CSharpCodeBuilderTests
 
         _ = await Assert.That(result).IsEqualTo(builder);
         _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithValidParameters_Should_AppendExceptionElement()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException("ArgumentNullException", "Thrown when argument is null");
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert
+            .That(builder.ToString())
+            .IsEqualTo(
+                "/// <exception cref=\"ArgumentNullException\">Thrown when argument is null</exception>"
+                    + Environment.NewLine
+            );
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithNullExceptionType_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException(null, "Exception description");
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithEmptyExceptionType_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException("", "Exception description");
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithNullDescription_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException("ArgumentNullException", null);
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithEmptyDescription_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException("ArgumentNullException", "");
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithBothNullParameters_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException(null, null);
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithGenericExceptionType_Should_AppendCorrectly()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException("InvalidOperationException", "Thrown when the operation is invalid");
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert
+            .That(builder.ToString())
+            .IsEqualTo(
+                "/// <exception cref=\"InvalidOperationException\">Thrown when the operation is invalid</exception>"
+                    + Environment.NewLine
+            );
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithFullyQualifiedType_Should_AppendCorrectly()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException(
+            "System.ArgumentOutOfRangeException",
+            "Thrown when the value is out of range"
+        );
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert
+            .That(builder.ToString())
+            .IsEqualTo(
+                "/// <exception cref=\"System.ArgumentOutOfRangeException\">Thrown when the value is out of range</exception>"
+                    + Environment.NewLine
+            );
+    }
+
+    [Test]
+    public async Task AppendXmlDocExceptions_WithValidExceptions_Should_AppendAllExceptionElements()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exceptions = new List<(string Type, string Description)>
+        {
+            ("ArgumentNullException", "Thrown when argument is null"),
+            ("InvalidOperationException", "Thrown when operation is invalid"),
+            ("ArgumentOutOfRangeException", "Thrown when value is out of range"),
+        };
+
+        var result = builder.AppendXmlDocExceptions(exceptions);
+
+        var expected =
+            "/// <exception cref=\"ArgumentNullException\">Thrown when argument is null</exception>"
+            + Environment.NewLine
+            + "/// <exception cref=\"InvalidOperationException\">Thrown when operation is invalid</exception>"
+            + Environment.NewLine
+            + "/// <exception cref=\"ArgumentOutOfRangeException\">Thrown when value is out of range</exception>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExceptions_WithSingleException_Should_AppendSingleExceptionElement()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exceptions = new List<(string Type, string Description)>
+        {
+            ("ArgumentNullException", "Thrown when argument is null"),
+        };
+
+        var result = builder.AppendXmlDocExceptions(exceptions);
+
+        var expected =
+            "/// <exception cref=\"ArgumentNullException\">Thrown when argument is null</exception>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExceptions_WithNullCollection_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocExceptions(null);
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExceptions_WithEmptyCollection_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exceptions = Array.Empty<(string Type, string Description)>();
+
+        var result = builder.AppendXmlDocExceptions(exceptions);
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExceptions_WithInvalidExceptions_Should_SkipInvalidEntries()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exceptions = new List<(string Type, string Description)>
+        {
+            ("ArgumentNullException", "Valid exception"),
+            (null!, "Invalid - null type"),
+            ("", "Invalid - empty type"),
+            ("InvalidOperationException", null!),
+            ("ArgumentException", ""),
+            ("ValidException", "Another valid exception"),
+        };
+
+        var result = builder.AppendXmlDocExceptions(exceptions);
+
+        var expected =
+            "/// <exception cref=\"ArgumentNullException\">Valid exception</exception>"
+            + Environment.NewLine
+            + "/// <exception cref=\"ValidException\">Another valid exception</exception>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExceptions_WithAllInvalidExceptions_Should_NotAppendAnything()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exceptions = new List<(string Type, string Description)>
+        {
+            (null!, "Invalid - null type"),
+            ("", "Invalid - empty type"),
+            ("SomeException", null!),
+            ("AnotherException", ""),
+        };
+
+        var result = builder.AppendXmlDocExceptions(exceptions);
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(string.Empty);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExceptions_Should_SupportMethodChaining()
+    {
+        var builder = new CSharpCodeBuilder();
+        var exceptions = new List<(string Type, string Description)>
+        {
+            ("ArgumentNullException", "Thrown when argument is null"),
+            ("InvalidOperationException", "Thrown when operation is invalid"),
+        };
+
+        var result = builder
+            .AppendXmlDocSummary("Method summary")
+            .AppendXmlDocExceptions(exceptions)
+            .AppendXmlDocReturns("Return value");
+
+        var expected =
+            "/// <summary>"
+            + Environment.NewLine
+            + "/// Method summary"
+            + Environment.NewLine
+            + "/// </summary>"
+            + Environment.NewLine
+            + "/// <exception cref=\"ArgumentNullException\">Thrown when argument is null</exception>"
+            + Environment.NewLine
+            + "/// <exception cref=\"InvalidOperationException\">Thrown when operation is invalid</exception>"
+            + Environment.NewLine
+            + "/// <returns>Return value</returns>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocExceptions_WithIndentation_Should_RespectIndentation()
+    {
+        var builder = new CSharpCodeBuilder();
+        builder.IncrementIndent();
+        var exceptions = new List<(string Type, string Description)>
+        {
+            ("ArgumentNullException", "Thrown when argument is null"),
+            ("InvalidOperationException", "Thrown when operation is invalid"),
+        };
+
+        var result = builder.AppendXmlDocExceptions(exceptions);
+
+        var expected =
+            "    /// <exception cref=\"ArgumentNullException\">Thrown when argument is null</exception>"
+            + Environment.NewLine
+            + "    /// <exception cref=\"InvalidOperationException\">Thrown when operation is invalid</exception>"
+            + Environment.NewLine;
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert.That(builder.ToString()).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task AppendXmlDocException_WithSpecialCharactersInDescription_Should_AppendCorrectly()
+    {
+        var builder = new CSharpCodeBuilder();
+
+        var result = builder.AppendXmlDocException(
+            "ArgumentException",
+            "Thrown when value contains <, >, &, or \" characters"
+        );
+
+        _ = await Assert.That(result).IsEqualTo(builder);
+        _ = await Assert
+            .That(builder.ToString())
+            .IsEqualTo(
+                "/// <exception cref=\"ArgumentException\">Thrown when value contains <, >, &, or \" characters</exception>"
+                    + Environment.NewLine
+            );
     }
 }
